fbd7a010b341940a23925e69edab8f03e567dd75
Ivo <32270959+Ivo8601@users.noreply.github.com>
2019-05-19 14:04:31 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -85,7 +85,7 @@ function getMovie() {
                 <h3>Plot</h3>
                 ${movie.Plot}
                 <hr>
-                <a href="http://imdb.com/title/${
+                <a href="https://imdb.com/title/${
                   movie.imdbID
                 }" target="_blank" class="btn btn-primary">View IMDB</a>
                 <a href="index.html" class="btn btn-default">Go Back To Search</a>
